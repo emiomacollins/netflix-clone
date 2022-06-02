3fe11c9a518bc69fbdbeac3c10f5466018cb7f96
Emioma collins <emiomacollins96@gmail.com>
2022-06-02 12:27:50 -0400

    fetch home page data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import styled from 'styled-components';
 import Hero from '../components/Hero';
 import Nav from '../components/Nav';
+import { baseAxios } from '../constants/api';
+import { Props } from './types';
 
-const Home: NextPage = () => {
+export default function Home(props: Props) {
+	const { netflixOriginals } = props;
+
 	return (
 		<Container>
 			<Head>
@@ -13,12 +16,43 @@ const Home: NextPage = () => {
 			</Head>
 
 			<Nav />
-
-			<Hero />
+			<Hero netflixOriginals={netflixOriginals} />
 		</Container>
 	);
-};
+}
 
-export default Home;
+const Container = styled.div`
+	height: 500vh;
+`;
 
-const Container = styled.div``;
+export async function getServerSideProps() {
+	const requests = [
+		`/trending/all/week`,
+		`/movie/top_rated`,
+		`/discover/movie?with_networks=213`,
+		`/discover/movie?with_genres=28`,
+		`/discover/movie?with_genres=35`,
+		`/discover/movie?with_genres=27`,
+		`/discover/movie?with_genres=10749`,
+		`/discover/movie?with_genres=99`,
+	];
+
+	const requestPromises = requests.map((request) => baseAxios(request));
+	const res = await Promise.all(requestPromises);
+	const data = res.map((res) => res.data.results);
+
+	const props: Props = {
+		trending: data[0],
+		topRated: data[1],
+		netflixOriginals: data[2],
+		actionMovies: data[3],
+		comedyMovies: data[4],
+		horrorMovies: data[5],
+		romanceMovies: data[6],
+		documentaries: data[7],
+	};
+
+	return {
+		props,
+	};
+}
