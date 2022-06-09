4cb4b95df49c5db0868e0f20c8dd7735bae08e56
Emioma collins <emiomacollins96@gmail.com>
2022-06-08 23:38:09 -0400

    modal

--- components/styled components/Overlay.tsx
+++ components/styled components/Overlay.tsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
-export const Overlay = styled.div`
+
+interface Props {
+	opacity?: number;
+}
+
+export const Overlay = styled.div<Props>`
 	position: fixed;
 	top: 0;
 	left: 0;
 	width: 100%;
 	height: 100%;
-	background: var(--dark-transparent);
+	background: ${(p) =>
+		p.opacity ? `rgba(0, 0, 0, ${p.opacity})` : 'var(--dark-transparent)'};
 `;
