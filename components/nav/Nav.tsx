d0f8c2b2d02a34a97ad18743ca0fd77b03551a1c
Emioma collins <emiomacollins96@gmail.com>
2022-06-16 17:51:38 -0400

    check nav issue

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -27,9 +27,9 @@ export default function Nav() {
 	const [scrolled, setScrolled] = useState(false);
 
 	useEffect(() => {
-		const handleScroll = () => setScrolled(window.scrollY > 0);
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
+		// const handleScroll = () => setScrolled(window.scrollY > 0);
+		// window.addEventListener('scroll', handleScroll);
+		// return () => window.removeEventListener('scroll', handleScroll);
 	}, []);
 
 	return (
